57e42fcc5b54945685c19f301da1e6b6fd2684a9
Rafi <rafiabyantarapratama@gmail.com>
2026-03-04 09:43:46 +0700

    Praktikum 2 js 5

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,22 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/', [WelcomeController::class, 'index']);
+
+// Route untuk Level & Kategori
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
 
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/id', [UserController::class, 'ubah']);
-
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route untuk User Management
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/tambah', [UserController::class, 'tambah']);
+    Route::post('/tambah_simpan', [UserController::class, 'tambah_simpan']);
+    Route::get('/ubah/{id}', [UserController::class, 'ubah']); // Pastikan pakai {id}
+    Route::put('/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+    Route::get('/hapus/{id}', [UserController::class, 'hapus']);
+});
