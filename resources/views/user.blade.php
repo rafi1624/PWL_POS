8b96e73ce720c201324b936eb7a9b07a6d3e0412
Rafi <rafiabyantarapratama@gmail.com>
2026-03-04 08:32:12 +0700

    praktikum 2.6 js 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,15 +3,26 @@
     <head>
         <title>Data User</title>
     </head>
-    <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>Jumlah Pengguna (Level ID: 2)</th>
-            </tr>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
-        </table>
-    </body>
+<body>
+    <h1>Data User</h1>
+    <a href="/user/tambah">+ Tambah User</a>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+        </tr>
+        @endforeach
+    </table>
+</body>
 </html>
