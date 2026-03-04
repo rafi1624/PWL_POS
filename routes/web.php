f3cbd84828a34998077189b498d50e7727542851
Rafi <rafiabyantarapratama@gmail.com>
2026-03-04 07:12:43 +0700

    Praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+<?php
 
-Route::get('/', [HomeController::class, 'index']);
+use App\Http\Controllers\LevelController;
+use Illuminate\Support\Facades\Route;
 
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'food']);
-    Route::get('/beauty-health', [ProductController::class, 'beauty']);
-    Route::get('/home-care', [ProductController::class, 'home']);
-    Route::get('/baby-kid', [ProductController::class, 'baby']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
