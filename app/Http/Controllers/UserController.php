f702ed3c85234e3063e331444159f9536a01dcdf
Rafi <rafiabyantarapratama@gmail.com>
2026-03-04 07:22:38 +0700

    Praktikum-6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-public function profile($id, $name) {
-    return view('user.profile', compact('id', 'name'));
-}
+    public function index()
+    {
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
+
+        // ubah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
